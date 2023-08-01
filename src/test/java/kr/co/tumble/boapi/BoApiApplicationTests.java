aa0d80f1e6fe7ca5f5b1c928838f5bbe7e1a7acc
palaoh <palaoh@plateer.com>
2023-08-01 11:20:31 +0900

    init

---
+++ src/test/java/kr/co/tumble/boapi/BoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.tumble.boapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
